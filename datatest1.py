1841fe8821796a17a3d96115e08b312c3201bb82
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2015-02-03 12:17:46 -0500

    Modified text files.

---
+++ datatest1.py
@@ -0,0 +1,15 @@
+import dataio as dio
+import thinkstats2 as ts2
+import thinkplot as tplt
+
+h155 = dio.importdata('h155_processed.pkl')
+h152a = dio.importdata('h152a_processed.pkl')
+
+print list(h155)
+
+# favvar = 'WHNHEL42'
+
+# hist = ts2.Hist(h155[favvar])
+# plothist = tplt.Hist(hist)
+# tplt.Show()
+# print 'end'
