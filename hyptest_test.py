eaca240ad414c7c38963cb0b4963273631112889
Samantha Kumarasena <samantha.kumarasena@students.olin.edu>
2015-02-22 23:27:48 -0500

    Adding documentation to hypothesis test

--- hyptest_test.py
+++ hyptest_test.py
@@ -5,17 +5,29 @@ import dataio
 
 
 class DiffMeansPermute(thinkstats2.HypothesisTest):
+    """Performs difference-of-means hypothesis test. 
+    """
     def TestStatistic(self, data):
+        """Calculates test statistic (in this case, difference of means)
+                data: tuple containing two datasets (ex. dataframes, lists) 
+                Returns: float difference in means
+        """
         group1, group2 = data
         test_stat = abs(group1.mean() - group2.mean())
         return test_stat
 
     def MakeModel(self):
+        """Creates model for hypothesis test -- combines both groups within
+            data to create a pool. 
+        """
         group1, group2 = self.data
         self.n, self.m = len(group1), len(group2)
         self.pool = np.hstack((group1, group2))
 
     def RunModel(self):
+        """Runs model for hypothesis test -- creates two groups from model 
+            to form the null hypothesis model. 
+        """
         np.random.shuffle(self.pool)
         data = self.pool[:self.n], self.pool[self.m:]
         return data
