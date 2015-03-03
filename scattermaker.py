8c37f0eb4b999d932ef499b7015b6ec3171bf4e9
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2015-03-03 11:47:22 -0500

    Fix.

---
+++ scattermaker.py
@@ -0,0 +1,48 @@
+# scattermaker.py
+import thinkstats2 as ts2
+import thinkplot as tplt
+from DataSet import DataSet
+import pandas as pd
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+from VarSearch import search_vars
+
+H155 = DataSet('h155.pkl')
+df = H155.df
+
+diagnoses = search_vars('diagnosis', H155.varnames)
+
+xvariable = 'OBTOTV12'
+xdescr = 'Number of office-based provider visits'
+yvariable = 'TOTSLF12'
+ydescr = 'Total Yearly Out-of-Pocket Healthcare Expenses'
+
+
+groups = []
+for diagnosis in diagnoses:
+    afflicted = df[
+        (df[diagnosis] == 1) & (df[xvariable] > 0) & (df[yvariable] > 0)]
+    xvar = afflicted[xvariable]
+    yvar = afflicted[yvariable]
+    assert len(xvar) == len(yvar)
+    data = {'name': diagnosis, 'x': xvar, 'y': yvar, 'xname': xvariable,
+            'yname': yvariable, 'xlabel': xdescr, 'ylabel': ydescr}
+    groups.append(data)
+
+num_sets = len(groups)
+
+colors = cm.jet(np.linspace(0, 1, num_sets))
+print colors
+
+for data, c in zip(groups, colors):
+    tplt.Scatter(data['x'], data['y'], color=c, label=data['name'])
+tplt.Config(ylabel=groups[0]['ylabel'], xlabel=groups[0][
+            'xlabel'], legend=True, loc='upper left')
+plt.show()  # tplt.Show() moves overrides legend pos. for some reason.
+
+# Correlations
+for data in groups:
+    thiscr = ts2.Corr(data['x'], data['y'])
+    print 'The correlation between %s and %s for %s is %5f' % (data['xname'], data['yname'], data['name'], thiscr)
