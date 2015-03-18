e5c8a8502b2fa85ce43e23849f2e530f792bb438
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2015-03-18 02:12:29 -0500

    Inital scaffolding for outliers.py

---
+++ MEPS/outliers.py
@@ -0,0 +1,37 @@
+# outliers.py
+
+from meps.data import DataSet
+import meps.think.stats2 as ts2
+import meps.think.plot as tplt
+
+import sklearn
+
+
+def identifyoutliers(dataset, variablename, highrange=10, lowrange=10):
+    """Given a dataframe and a variable name, return a dictionary
+    of low and high outliers identified by by DUPERSID.
+    "lowrange" and "highrange" specify percent ranges of outliers. (e.g.
+    lowrange=5 will return the lowest 5% of data in the low outliers set.)
+    """
+    # Create histogram
+
+    # Find value of lowrange-th percentile
+
+    # Identify all points in 0-lowrange percentiles.
+
+    pass
+
+
+def makedataframe(dataset, outliers):
+    """Given a dataframe and a list of outlier DUPERSIDs, returns a dataframe
+    containing only rows associated with the DUPERSIDs in 'outliers'.
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
