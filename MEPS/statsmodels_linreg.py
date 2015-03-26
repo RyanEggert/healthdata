5586a9a63226509da45e5698cecb2c5a19f23db8
Samantha Kumarasena <samantha.kumarasena@students.olin.edu>
2015-03-26 17:28:32 -0400

    New regression model, same group. R^2 = 0.83. Some questions about certain variables and their strange effects on R^2.

--- MEPS/statsmodels_linreg.py
+++ MEPS/statsmodels_linreg.py
@@ -10,11 +10,19 @@ import meps.think.plot as tplt
 
 dependent = 'TOTEXP12'
 #independents = ['BMINDX53', 'AGE12X', 'C(HIDEG)']
-independents = ['C(NOASPR53)', 'C(CHNEVER)', 'OHRTAGED', 'CHDAGED', 'ADHECR42', 'ADSPRF42', 'ADILWW42' ] #, 'C(RTHLTH31)', 'AGE12X','DENTCK53', 'ADLIST42' 
+independents = ['C(NOASPR53)', 'C(CHOLCK53)', 'OHRTAGED', 'CHDAGED', 'C(ADHECR42)', 'C(ADSPRF42)', 'C(ADILWW42)', 'C(DECIDE42)'] #, 'C(RTHLTH31)', 'AGE12X','DENTCK53', 'ADLIST42', 'C(RESPCT42)' 
 #'C(CHOLCK53)'
 
+#including 'C(LANGPR42)' made R2 1, everything nan...what?
 
-#R2 went down from .64 to .51 when added 'ADEZUN42'... what?
+#RESPCT42: Does provider ask about and show respect for medical, traditional, and alternative treatments that the person is happy with 
+#DECIDE42: Does provider ask the person to help make decisions between a choice of treatments (DECIDE42)
+#ADILWW42: SAQ 12 Mos: Got Care When Needed Ill/Inj
+#ADHECR42: SAQ 12 Mos: Rating of Health care
+#ADSPRF42: SAQ 12Mos: How Esy Getting Spec Referral IF needed to see a specialist
+
+
+#R2 went down from .64 to .51 when added 'ADEZUN42'
 #C(OHRTAGED) increases R2 to 
 
 #note: collinearity between AGE and HIDEG
@@ -43,4 +51,5 @@ results_all = model_all.fit()
 results_hichol = model_hichol.fit()
 
 #print results_all.rsquared
+#print results_hichol.rsquared
 print results_hichol.summary()
