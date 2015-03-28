52563b2643b51617db569d26a0e6cf6666269244
Samantha Kumarasena <samantha.kumarasena@students.olin.edu>
2015-03-28 17:16:14 -0400

    Realized we were doing everything wrong with previous models. Fixing the models to be more statistically rigorous :P new model of TOTEXP12 with high-cholesterol diagnosis, R^2 = .46

--- MEPS/statsmodels_linreg.py
+++ MEPS/statsmodels_linreg.py
@@ -12,9 +12,13 @@ dependent = 'TOTEXP12'
     #WHOLE POPULATION
 #independents = ['OHRTAGED', 'CHDAGED', 'C(ADHECR42)', 'C(ADSPRF42)', 'C(ADILWW42)', 'C(DECIDE42)']
     #HIGH CHOLESTEROL
-#independents = ['C(NOASPR53)', 'C(CHOLCK53)', 'OHRTAGED', 'CHDAGED', 'C(ADHECR42)', 'C(ADSPRF42)', 'C(ADILWW42)', 'C(DECIDE42)'] #, 'C(RTHLTH31)', 'AGE12X','DENTCK53', 'ADLIST42', 'C(RESPCT42)' 
+independents = ['C(BADHLTH)', 'IPNGTD12' ,'C(PREGNT31)', 'C(INSAT12X)', 'C(ARTHDX)'] #, 'C(RTHLTH31)', 'AGE12X','DENTCK53', 'ADLIST42', 'C(RESPCT42)' 'C(ADILWW42)', 'C(ADSPRF42)''BMINDX53', 'AGE12X', 
+#AGE12X and ARTHDX are interchangeable! Switched them out, had same R^2 value.     
+#None of the condition variables affect total cost aside from ARTHDX
+
+
     #CORONARY HEART DISEASE
-independents = ['C(NOASPR53)', 'OHRTAGED','CHDAGED', 'C(ADHECR42)', 'C(ADSPRF42)', 'C(ADILWW42)', 'C(DECIDE42)']
+#independents = ['C(NOASPR53)', 'OHRTAGED','CHDAGED', 'C(ADHECR42)', 'C(ADSPRF42)', 'C(ADILWW42)', 'C(DECIDE42)']
 
 #including 'C(LANGPR42)' made R2 1, everything nan...what?
 
@@ -33,11 +37,17 @@ h155 = DataSet('h155.pkl')
 df = h155.df
 df = cleanallerrs(df)
 
-#df['CHNEVER'] = df['CHOLCK53'] >= 3 
+df['BADHLTH'] = df['RTHLTH31'] >= 4
+df['EXPHLTH'] = 10**(df['RTHLTH31']**2)
+df['PROBLEM'] = df['MDUNPR42'] == 1
+
 
 
 hichol = df[df.CHOLDX == 1]
 cheartd = df[df.CHDDX == 1]
+
+
+
 
 formula = dependent +  ' ~ ' + independents[0]
 
@@ -56,5 +66,5 @@ results_cheartd = model_cheartd.fit()
 
 #print results_all.rsquared
 #print results_hichol.rsquared
+#print results_cheartd.summary()
 print results_cheartd.summary()
-#print results_hichol.summary()
