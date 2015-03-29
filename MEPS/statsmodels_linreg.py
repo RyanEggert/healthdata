33651b65b9ed78195876b52bfad195a32b69c331
Samantha Kumarasena <samantha.kumarasena@students.olin.edu>
2015-03-29 01:26:20 -0400

    Updating model

--- MEPS/statsmodels_linreg.py
+++ MEPS/statsmodels_linreg.py
@@ -12,9 +12,19 @@ dependent = 'TOTEXP12'
     #WHOLE POPULATION
 #independents = ['OHRTAGED', 'CHDAGED', 'C(ADHECR42)', 'C(ADSPRF42)', 'C(ADILWW42)', 'C(DECIDE42)']
     #HIGH CHOLESTEROL
-independents = ['C(BADHLTH)', 'IPNGTD12' ,'C(PREGNT31)', 'C(INSAT12X)', 'C(ARTHDX)'] #, 'C(RTHLTH31)', 'AGE12X','DENTCK53', 'ADLIST42', 'C(RESPCT42)' 'C(ADILWW42)', 'C(ADSPRF42)''BMINDX53', 'AGE12X', 
+independents = ['C(BADHLTH)', 'C(INS12X)', 'IPNGTD12', 'C(ARTHDX)', 'ADAPPT42', 'AGE12X', 'BMINDX53', 'C(RICH)', 'C(COGLIM31)'] #, 'POVLEV12', 'C(RTHLTH31)', 'AGE12X','DENTCK53', 'ADLIST42', 'C(RESPCT42)' 'C(ADILWW42)', 'C(ADSPRF42)''BMINDX53', 'AGE12X', 
 #AGE12X and ARTHDX are interchangeable! Switched them out, had same R^2 value.     
 #None of the condition variables affect total cost aside from ARTHDX
+
+#what do all the numbers mean?
+#BMI relationship...what is it?
+#perceived health -- what is it?
+#age + bmi or total prescriptions?
+
+
+#INSAT12X, INS12X
+#, ,   'BMINDX53'
+
 
 
     #CORONARY HEART DISEASE
@@ -40,8 +50,12 @@ df = cleanallerrs(df)
 df['BADHLTH'] = df['RTHLTH31'] >= 4
 df['EXPHLTH'] = 10**(df['RTHLTH31']**2)
 df['PROBLEM'] = df['MDUNPR42'] == 1
+df['CHOLCKYR'] = df['CHOLCK53'] == 1
+df['BPCHKYR'] = df['BPCHEK53'] == 1
+df['RICH'] = df['POVCAT12'] ==5
 
-
+#group = df[(df.CHOLDX == 1) & (df.CHOLCK53 == 5)]
+#print len(group['DUPERSID'].values)
 
 hichol = df[df.CHOLDX == 1]
 cheartd = df[df.CHDDX == 1]
@@ -58,13 +72,13 @@ print formula
 
 model_all = smf.ols(formula, data=df)
 model_hichol = smf.ols(formula, data=hichol)
-model_cheartd = smf.ols(formula, data=cheartd)
+#model_cheartd = smf.ols(formula, data=cheartd)
 
 results_all = model_all.fit()
 results_hichol = model_hichol.fit()
-results_cheartd = model_cheartd.fit()
+#results_cheartd = model_cheartd.fit()
 
 #print results_all.rsquared
 #print results_hichol.rsquared
 #print results_cheartd.summary()
-print results_cheartd.summary()
+print results_hichol.summary()
